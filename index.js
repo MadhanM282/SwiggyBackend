64d865a81728390eb948d547cd4423b040ff133a
MadhanM282 <madhanm282@gmail.com>
2022-03-29 21:45:52 +0530

    make it better

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ const Port = process.env.PORT
 
 const Restaurent = require('./src/controller/controller')
 
-// const ITEM = require('./src/controller/item')
+const ITEM = require('./src/controller/item')
 
 const connect = require('./src/base/db')
 
 app.use(express.json())
 
 app.use("/rest",Restaurent);
+
+app.use("/item",ITEM)
 
 app.listen(Port||3090, async (req, res)=>{
     try{
